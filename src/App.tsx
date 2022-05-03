33ac7f2d7312a0c51b2531f20488039be17d02ba
Thiago Martins <>
2022-05-03 12:55:27 -0300

    Adcionando componente Widget e variavel de estado

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,7 @@
-interface ButtonProps{
-  text:   string;
-  name?:  string; //opcional
+import { Widget } from "./components/Widget";
 
-}
-function Button(props:ButtonProps){
-  console.log(props.text)
-  return <button className="buttostyle hover:bg-violet-700 transition-colors">{props.text}</button>
-
-}
-function App() { 
-  return (
-
-    <div className="flex gap-2">
-
-    <Button text = 'Enviar'/>
-    <Button text = 'Cancelar'/>
-    </div>
-    )
+export function App() { 
+  return  <Widget />
+    
   }
-  
 
-export default App
