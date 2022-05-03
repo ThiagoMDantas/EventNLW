5a82d4e13c433468325f49cdc07f4795ef255448
Thiago Martins <>
2022-05-03 16:13:07 -0300

    substituindo controle de estado useState pelo controlador de estado do Headless

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,36 +1,44 @@
 import {ChatTeardropDots} from 'phosphor-react';
 import { useState } from 'react';
+import { Popover } from '@headlessui/react'
 
 export function Widget(){
 
+//Comentado para implantar o controle de estado do Popover
   //1- Criando uma variavel de estado com o valor default false
   //2- isWidgetOpen váriavel de estádo
   //3- Função responsável por alterar o estado da variavel
-  const [isWidgetOpen, setWidgetOpen] = useState(false)
+  //const [isWidgetOpen, setWidgetOpen] = useState(false)
 
   //4- Função que chama a função que altera o valor da variavel de estado
-  function toggleWidgetVisibility(){
-    setWidgetOpen(!isWidgetOpen)
-  }
+  
+  //function toggleWidgetVisibility(){
+  // setWidgetOpen(!isWidgetOpen)
+  //}
 
   return (
-    <div className="absolute bottom-5 right-5">
+
+    //implantando headless Popover
+    <Popover className="absolute bottom-5 right-5">
 
       
-      {//iniciando um bloco javascript para verificar o estado da variavel de estado e exibir a mensagem
+      {
+      //iniciando um bloco javascript para verificar o estado da variavel de estado e exibir a mensagem
       //usando operador ternário (if condição ? true : false)
-      isWidgetOpen ? <p>Hello Woold</p> : null}
-
-      <button onClick={toggleWidgetVisibility} className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
-        <ChatTeardropDots className='w-6 h-6' />
-
-
-        <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-700 ease-linear">
-          <span className="pl-2">
-            Feedback
-          </span>
-        </span>
-      </button>
-    </div>
+      //Comentado para implantar o controle de estado do Popover
+      //isWidgetOpen ? <p>Hello Woold</p> : null}
+    }
+        
+          <Popover.Panel>Hello Woold</Popover.Panel>
+          
+          <Popover.Button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
+            <ChatTeardropDots className='w-6 h-6' />
+            <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-700 ease-linear">
+              <span className="pl-2">
+                Feedback
+              </span>
+            </span>
+          </Popover.Button>
+    </Popover>
   )
 }
